4ae2b6b18972fc6a7bdab3558ea4e25544aee5d8
Al Fattaul Islam <sahilabrar444@gmail.com>
2024-12-19 11:02:50 +0600

    added isBlocked function

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,7 +1,19 @@
+import { UserModel } from '../user/user.model';
 import { iAuth } from './auth.interface';
+import { AppError } from './../../errors/AppErrors';
 
 const loginUser = async (payload: iAuth) => {
-  console.log(payload);
+  const isUserExists = await UserModel.findOne({ id: payload.id });
+
+  if (!isUserExists) {
+    throw new AppError(404, 'User does not exist', '');
+  }
+
+  const isBlocked = isUserExists?.isBlocked;
+
+  if (isBlocked === true) {
+    throw new AppError(404, 'User Is Blocked', '');
+  }
   return {};
 };
 
