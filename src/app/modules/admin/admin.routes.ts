0a8018bc58cb9f0fc826700354624ffc9f7e520a
Al Fattaul Islam <sahilabrar444@gmail.com>
2024-12-21 20:18:58 +0600

    added auth verify

--- src/app/modules/admin/admin.routes.ts
+++ src/app/modules/admin/admin.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import { adminController } from './admin.controller';
+import { Auth } from '../../middlewares/auth';
+import { USER_ROLE } from '../user/user.constants';
 
 const adminRouter = Router();
 
-adminRouter.post('/users/:userId/block', adminController.blockUser);
+adminRouter.post(
+  '/users/:userId/block',
+  Auth(USER_ROLE.ADMIN),
+  adminController.blockUser,
+);
 
 export default adminRouter;
